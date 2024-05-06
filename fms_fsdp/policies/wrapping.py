77a2211d3818d28885607f381f874157af338998
Linsong Chu <lchu@us.ibm.com>
2024-05-06 15:04:19 -0400

    wrap word embed as fsdp unit

--- fms_fsdp/policies/wrapping.py
+++ fms_fsdp/policies/wrapping.py
@@ -1,14 +1,14 @@
 import functools
 
-from fms.modules.embedding import WordEmbedding
 from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
+from torch.nn import Embedding
 
 
 def get_wrapper(block):
     auto_wrap_policy = functools.partial(
         transformer_auto_wrap_policy,
         transformer_layer_cls={
-            block, WordEmbedding
+            block, Embedding
         },
     )
 

3994ce62da78442452c2f8a66a078831efc2e3e3
Linsong Chu <lchu@us.ibm.com>
2024-06-18 13:21:03 -0400

    up

--- fms_to_hf.py
+++ fms_to_hf.py
@@ -1,17 +1,33 @@
 import fire
-from torch.distributed._shard.checkpoint import FileSystemReader, load_state_dict
 from mamba_ssm.models.config_mamba import MambaConfig
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from torch.distributed._shard.checkpoint import FileSystemReader, load_state_dict
 
 
 def main(load_path, save_path, tokenizer_name_or_path):
     print("Initializing model...")
-    config_data = {'d_model': 2048, 'd_intermediate': 0, 'n_layer': 48, 'vocab_size': 128256,
-                   'ssm_cfg': {'layer': 'Mamba2'}, 'attn_layer_idx': [8, 16, 24, 32, 40],
-                   'attn_cfg': {'causal': True, 'd_conv': 4, 'head_dim': 128, 'num_heads': 24, 'out_proj_bias': False,
-                                'qkv_proj_bias': False, 'rotary_emb_dim': 64}, 'rms_norm': True,
-                   'residual_in_fp32': True, 'fused_add_norm': True, 'pad_vocab_size_multiple': 16,
-                   'tie_embeddings': True}
+    config_data = {
+        "d_model": 2048,
+        "d_intermediate": 0,
+        "n_layer": 48,
+        "vocab_size": 128256,
+        "ssm_cfg": {"layer": "Mamba2"},
+        "attn_layer_idx": [8, 16, 24, 32, 40],
+        "attn_cfg": {
+            "causal": True,
+            "d_conv": 4,
+            "head_dim": 128,
+            "num_heads": 24,
+            "out_proj_bias": False,
+            "qkv_proj_bias": False,
+            "rotary_emb_dim": 64,
+        },
+        "rms_norm": True,
+        "residual_in_fp32": True,
+        "fused_add_norm": True,
+        "pad_vocab_size_multiple": 16,
+        "tie_embeddings": True,
+    }
     mamba_config = MambaConfig(**config_data)
     model = MambaLMHeadModel(mamba_config)
 
--- main_training.py
+++ main_training.py
@@ -62,12 +62,50 @@ def main(**kwargs):
     ) = get_policies(cfg, rank, block)
 
     # get fms model
-    config_data = {'d_model': 2048, 'd_intermediate': 0, 'n_layer': 48, 'vocab_size': 128256,
-                   'ssm_cfg': {'layer': 'Mamba2'}, 'attn_layer_idx': [8, 16, 24, 32, 40],
-                   'attn_cfg': {'causal': True, 'd_conv': 4, 'head_dim': 128, 'num_heads': 24, 'out_proj_bias': False,
-                                'qkv_proj_bias': False, 'rotary_emb_dim': 64}, 'rms_norm': True,
-                   'residual_in_fp32': True, 'fused_add_norm': True, 'pad_vocab_size_multiple': 16,
-                   'tie_embeddings': True}
+    config_data = {
+        "d_model": 2048,
+        "d_intermediate": 0,
+        "n_layer": 48,
+        "vocab_size": 128256,
+        "ssm_cfg": {"layer": "Mamba2"},
+        "attn_layer_idx": [8, 16, 24, 32, 40],
+        "attn_cfg": {
+            "causal": True,
+            "d_conv": 4,
+            "head_dim": 128,
+            "num_heads": 24,
+            "out_proj_bias": False,
+            "qkv_proj_bias": False,
+            "rotary_emb_dim": 64,
+        },
+        "rms_norm": True,
+        "residual_in_fp32": True,
+        "fused_add_norm": True,
+        "pad_vocab_size_multiple": 16,
+        "tie_embeddings": True,
+    }
+    config_data = {
+        "d_model": 2560,
+        "d_intermediate": 0,
+        "n_layer": 64,
+        "vocab_size": 128256,
+        "ssm_cfg": {"layer": "Mamba2"},
+        "attn_layer_idx": [9, 18, 27, 36, 45, 56],
+        "attn_cfg": {
+            "causal": True,
+            "d_conv": 4,
+            "head_dim": 128,
+            "num_heads": 30,
+            "out_proj_bias": False,
+            "qkv_proj_bias": False,
+            "rotary_emb_dim": 64,
+        },
+        "rms_norm": True,
+        "residual_in_fp32": True,
+        "fused_add_norm": True,
+        "pad_vocab_size_multiple": 16,
+        "tie_embeddings": True,
+    }
     mamba_config = MambaConfig(**config_data)
     model = MambaLMHeadModel(mamba_config)
 

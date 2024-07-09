3b7c40059d999c67f2049ed9f5c4e97df9f10cab
Linsong Chu <lchu@us.ibm.com>
2024-07-08 22:37:27 -0400

    up

--- main_training.py
+++ main_training.py
@@ -108,7 +108,7 @@ def main(**kwargs):
 
     # Optimizer
     optimizer = optim.AdamW(
-        model.parameters(), lr=cfg.learning_rate, betas=(0.9, 0.95), weight_decay=0.1
+        model.parameters(), lr=cfg.learning_rate, betas=(0.9, 0.95)
     )
 
     # optionally load from checkpoint (when continue pretraining)
